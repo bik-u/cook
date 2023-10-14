199d19b835942f5d4daa515ee896cf2f71e8e700
biku <dulal.biku@gmail.com>
2023-10-14 01:16:51 -0500

    add subtitles

--- addon.js
+++ addon.js
@@ -3,9 +3,9 @@ require("dotenv").config()
 const axios  = require("axios")
 const ani_url = "https://aniwatch.to/"
 const kitsu_url = "https://kitsu.io/api/edge/anime/"
-const pink_url = "https://pinkish-hue.vercel.app/"
+const media_url = "https://megacloud.tv/embed-2/ajax/e-1/getSources?id="
+const crypto_js = require('crypto-js');
 
-console.log(process.env.TMDB_KEY)
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
 	"id": "community.aniwatch",
@@ -26,47 +26,31 @@ const manifest = {
 }
 const builder = new addonBuilder(manifest)
 
-function check_m_type(m_type, sub_count, dub_count) {
-	if (m_type == "sub") {
-		if (sub_count > 0) {
-			if (dub_count > 0) {
-				return false
-			}
-			return false
-		}
-	} else if (m_type == "dub") {
-		if (dub_count > 0) {
-			if (sub_count > 0) {
-				return false
-			}
-			return false
-		}
-	}
-	return true
-}
+async function decrypt(url) {
+	const decryptKey = await (
+        await axios.get(
+          "https://raw.githubusercontent.com/enimax-anime/key/e6/key.txt"
+        )
+      ).data;
+	let explodedURL = url.split('')
+    let key = ''
+    for (const range of decryptKey) {
+        for (let i = range[0]; i < range[1]; i++) {
+            key += explodedURL[i]
+            explodedURL[i] = ''
+        }
+    }
+    let final_url = explodedURL.join('')
+    return JSON.parse(crypto_js.AES.decrypt(final_url, key).toString(crypto_js.enc.Utf8))
+}	
 
-async function decrypt() {
-	const decrypt = await (
-      await axios.get("https://github.com/enimax-anime/key/blob/e6/key.txt")
-    ).data;
 
-    const blobString = '"blob-code blob-code-inner js-file-line">';
-    const afterIndex = decrypt.indexOf(
-      '"blob-code blob-code-inner js-file-line">'
-    );
-    const newblobString =
-      afterIndex == -1 ? "" : decrypt.substring(afterIndex + blobString.length);
-    const beforeIndex = newblobString.indexOf("</td>");
-    let decryptKey =
-      beforeIndex == -1 ? "" : newblobString.substring(0, beforeIndex);
-	console.log(decryptKey)
-}
 
 builder.defineStreamHandler(async({type, id}) => {
 	console.log("request for streams: "+type+" "+id)
 	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineStreamHandler.md
 	const r = id.split(':')
-	const num = parseInt(r[r.length - 1])
+	const num = parseInt(r[r.length - 1]) - 1
 	let title = null
 	streams = []
 	await axios.get(url=kitsu_url + r[1],
@@ -97,20 +81,21 @@ builder.defineStreamHandler(async({type, id}) => {
 					if (id[s].length > 11) {
 						shows.push({
 							id: id[s].slice(0, -11),
-							title: arr.split('?')[0]
+							title: ''.concat(arr.split('?')[0].split('-'), ' ')
 						})
 					}
 				}
 
 			}
 		})
+
 	for (let i = 0; i < shows.length; i++) {
 		let show = shows[i]
 		let show_id = shows[i].id
 		await axios.get(url=`${ani_url}/ajax/v2/episode/list/${show_id}`)
 			.then(function(response) {
 				if (response.data.status) {
-					let titles = response.data.html.match(/title=".+/g)
+					let titles = response.data.html.match(/title=".+>/g)
 					const ids = response.data.html.match(/data-id=("[0-9]+")/g)
 					try {
 						show["episode"] = {
@@ -125,20 +110,65 @@ builder.defineStreamHandler(async({type, id}) => {
 		if (!show.episode) {
 			continue
 		}
-		await axios.get(url=`${pink_url}watch${show.title}?ep=${show.episode.id}`)
-			.then(function(response) {
-				let data = response.data[0]
-				for (let j = 0; j < data.sources.length; j++) {
-					let src = data.sources[j]
+		const response = await axios.get(url=`${ani_url}ajax/v2/episode/servers?episodeId=${show.episode.id}}`)
+		let subtitles = null
+
+		if (response.data.status) {
+			const matches = response.data.html.match(/data-type=.+"/g)
+			let sub = false
+			let dub = false
+			for (let i = 0; i < matches.length; i++) {
+				try {
+					const match = matches[i]
+					const arr = match.split('"')
+					const srv_id = arr[arr.length - 2]
+					const m_type = arr[1]
+					if (m_type == 'sub' && sub) {
+						continue
+					} else if (m_type == 'sub') {
+						sub = true
+					}
+					if (m_type == 'dub' && dub) {
+						continue
+					} else if (m_type == 'dub') {
+						dub = true
+					}
+					const response2 = await axios.get(`${ani_url}ajax/v2/episode/sources?id=${srv_id}`)
+					let src_id = response2.data.link.split('/')
+					src_id = src_id[src_id.length - 1].split('?')[0]
+					const response3 = await axios.get(`${media_url}${src_id}`)
+					const vid_data = await decrypt(response3.data.sources)
+					file = vid_data[0].file
+					if (!subtitles) {
+						const tracks = response3.data.tracks
+						subtitles = [] 
+						for(let j = 0; j < tracks.length; j++) {
+							let track = tracks[j]
+							if (!track.kind == "captions") {
+								continue
+							}
+							subtitles.push({
+								id: track.label,
+								url: track.file,
+								lang: track.label
+							})
+						}
+					}
 					streams.push({
-						title: `${show.episode.title} ${src.quality}`,
-						url: src.url
+						title: `${show.episode.title} | ${m_type.toUpperCase()}`,
+						url: vid_data[0].file,
+						subtitles: subtitles
 					})
+				} catch (error) {
+					console.log(error)
+
 				}
-			})
+			}
+			
+		}
+
 	}
 
-	await decrypt()
 	return {streams}
 })
 
--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 from Crypto.Cipher import AES
+import json
+import requests
 
-key = [[29,36],[83,90],[142,148],[152,159],[168,175]]
-src = b'U2FsdGVkX18Lt+aMDqATenComOwzIAzjBqPaor5ottBeANtJPg=ZYkaKEe0zdzVItGzcXb3qrlr4quWSlwwIQSstRL'
+key = json.loads(requests.get("https://raw.githubusercontent.com/enimax-anime/key/e6/key.txt").content)
+src = "U2FsdGVkX185iv51ubYXXz9+0cQ8LGvZaufHLrggypdpsomDQqPdGUO8AW0LLjHFnJ8h99fwKzzipBt4gXKPHEuKQtDauVSDqWLBfaUCI22u1sysDKHtCs94wtUji80RQto2dDM7mDM9mc/UWhDzwFznhrSJPxyeGnb5jHyhRjex7Gt3CtBgQvXAhje8YGTrlRcqAPaCxglXJOUJsiKGXi9jKBclcmutpNzFwjCiXHCSX3eGKHup7YEajnDEcMlQdY9YA0qSm/2Qc3rCXwdQUvcJ076HBhcMPAnvY6tkpLzSslANmhveePDyPFPxqGDfJ+RKPgOVG1uQKb3MX9pdmVWkLHHD1IaemLTlmlhqdVvLe3okhwVpF2FZOJf6CuO0sm3lTeOdp+m3XcDjOB93/vRKuSrnWaAtDOJNlR13PUAYPQaV7OLOI5OFrtnJg6MCRzXJsU6oad8/+2SFcJe2IRtSYng/xXcvkCF9lZc9bYy8m2Vd333Ug==U2FsdGVkX18Sxg4y8x7FP1eIe3iPRRWIoRj+3C/DMD842i0Q61qc/IXMWnb8ujHFnJ8B3Effnm8NfhTVjVXwIY1HNYmD+nbo3TRMpaUCI22u3kF+DKHtCsIDjKFd8ritzNo2dDM7xjSdhS6CDQFOA9B44KSJPxyeTzTWK4xskcgaC6Gm789IjKSwiZtM6MpAPJIhCXWFMXUykmX2y67PuZAPnqZ9mj/FtqTIaML9aUlNeOJ0UtxjdRbwtjA5b6oBlAdN+K22Rk+UhnDDvcrdraTUbBf3ZHUR9zuM0yGDmt5+u1hIJ6TK1+WrFJTxBP5i2KSusz1SVa4072MucfSFVS0zlpz26djTbCAlUFzZNk+VnLBXvPWeWVWpsa7bwel9813SKxsWrcuWEj2EV9buUrNxRnr5u2tF6z8RartS3pkV6ZW/fMQ8mOgh26UAEgiZ2cvmoRhiaB8iAoy9ZtEdtXNhURKbaj0TEqA7Mp3uBPdpCsw405yaA==" 
+
+src_arr = list(src)
+extracted_key = ""
+
+for i in range(len(key)):
+    for j in range(key[i][0], key[i][1]):
+        extracted_key+=src[j]
+        src_arr[j] = ""
+    
+key = extracted_key
+sources = "".join(src_arr)
+
+print(key, sources)
+    
 
 cipher = AES.new(key, AES.MODE_EAX)
-data = cipher.decrpt_and_verify(src)
+data = cipher.decrpt_and_verify(sources)
